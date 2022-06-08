6008fdb8d97ef7acd6d2752f68f8c306d353407b
Rutvik Patel <rutvikpatel90990@gmail.com>
2022-06-08 12:11:15 +0530

    add: 404 page

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-     404 not found 
+  <div class="not-found">
+    <h1>404</h1>
+    <h2>Page not found</h2>
+    <p>
+      The page you are looking for does not exist.
+    </p>
   </div>
 </template>
 
@@ -11,5 +15,8 @@ export default {
 </script>
 
 <style>
-
+.not-found {
+  text-align: center;
+  padding: 50px 0;
+}
 </style>
